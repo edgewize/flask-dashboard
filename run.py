a043d8eb1f6091cf3a8d40d29d4f0e456dee7d54
edgewize <tedginton88@gmail.com>
2020-04-11 17:45:33 +0000

    edits for running in python anywhere

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ from templates import app
 
 #Load this config object for development mode
 app.config.from_object('configurations.DevelopmentConfig')
-app.run(port=9999, threaded=True)
+if __name__ == '__main__':
+    app.run(port=9999, threaded=True)
